3f910366d616b8c5c9c4dd8402092b656d2e8517
lopesj <joao.lopes@studentlab.pt>
2021-12-28 22:33:07 +0000

    new version - already with some api calls

--- app.py
+++ app.py
@@ -1,17 +1,59 @@
+import json
+
 from flask import Flask, render_template, request
 from flask_migrate import Migrate
-from models import db, InfoModel
+from models import db, Chord, User, Song
+from better_profanity import profanity
+import sshtunnel
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@192.168.1.206:5432/p06_teste"
+'''
+tunnel = sshtunnel.SSHTunnelForwarder(
+    ('sigma.tecnico.ulisboa.pt'), ssh_username='ist193584', ssh_password='C8*Bdaz2',
+    remote_bind_address=('db.tecnico.ulisboa.pt', 5432)
+)
+
+tunnel.start()
+'''
+# IST DB
+#db_str = "postgresql://ist193584:124jotPOC@127.0.0.1:{}/ist193584".format(tunnel.local_bind_port)
+# RASPI DB
+db_str = "postgresql://postgres:postgres@192.168.1.206:5432/p06_canoa_3"
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_str
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
+profanity.load_censor_words()
+f = open("customBadWords.txt")
+profanity.add_censor_words(f.readlines())
+f.close()
 
-@app.route('/signup')
+
+@app.route('/')
+def home():
+    result = Chord.query.all()
+    return "Hello World"
+
+
+@app.route('/signup', methods=['POST', 'GET'])
 def form():
-    return render_template('signup.html')
+    if request.method == "GET":
+        return render_template('signup.html')
+    elif request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        scout_group = request.form['scout_group']
+        password = request.form['password']
+        if User.query.filter_by(email=email).first() != None:
+            return json.dumps({"result": "ERROR_DUPLICATE_EMAIL"})
+        new_user = User(first_name=first_name,last_name=last_name, email=email, password=password, scout_group=scout_group)
+        db.session.add(new_user)
+        db.session.commit()
+        return json.dumps({"result": "SUCCESS", "data": new_user.__repr__()})
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -19,26 +61,121 @@ def login():
     if request.method == 'GET':
         return "Login via the login Form"
 
-    if request.method == 'POST':
-        name = request.form['name']
-        age = request.form['age']
-        email = request.form['email']
-        password = request.form['password']
-        confirm_password = request.form['confirm_password']
-        if confirm_password != password:
-            return "Passwords did not match<br>Password: '{}'\n<br>Confirm password: '{}'".format(password,confirm_password)
-        query_name = InfoModel.query.filter_by(name=name).first()
-        query_email = InfoModel.query.filter_by(email=email).first()
-        if query_email != None and query_name != None and query_email.email == email and query_name.name == name:
-            return "Username '{}' and Email '{}' are already in use".format(name,email)
-        elif query_email != None and query_email.email == email:
-            return "Email '{}' is already in use".format(email)
-        elif query_name != None and query_name.name == name:
-            return "Username '{}' is already in use".format(name)
-        new_user = InfoModel(name=name, age=age, email=email, password=password)
-        db.session.add(new_user)
+@app.route('/new_song', methods=['POST', 'GET'])
+def new_song():
+    if request.method == 'GET':
+        return render_template('new_song_form.html')
+    elif request.method == 'POST':
+        title = request.form['title']
+        lyrics = request.form['lyrics']
+        lyrics_list = lyrics.split('\n')
+        if check_profanity([title,lyrics]):
+            return json.dumps({"result": "ERROR: WATCH_YOUR_PROFANITY"})
+        chords_list = {}
+        for i in range(len(lyrics_list)):
+            for j in range(len(lyrics_list[i])):
+                pos = "{};{}".format(i, j)
+                chords_list[pos] = None
+
+        #categories = request.form['categories']
+        #creator_id = request.form['creator_id']
+        new_song = Song(title=title,lyrics=json.dumps({'lyrics':lyrics_list}),chords_list=json.dumps(chords_list),categories=json.dumps({}),creator_id=1001)
+        db.session.add(new_song)
         db.session.commit()
-        return f"Done!!"
+        return json.dumps({"result": "SUCCESS", "data": new_song.__repr__()})
+
+
+@app.route('/load_chords')
+def load_chords():
+    if len(Chord.query.all()) != 0:
+        return "Chords already on db"
+    chords_list = [
+        Chord(name='Do Maior', representation='C', is_minor=0),
+        Chord(name='Do Sustenido Maior', representation='C#', is_minor=0),
+        Chord(name='Re Maior', representation='D', is_minor=0),
+        Chord(name='Re Sustenido Maior', representation='D#', is_minor=0),
+        Chord(name='Mi Maior', representation='E', is_minor=0),
+        Chord(name='Fa Maior', representation='F', is_minor=0),
+        Chord(name='Fa Sustenido Maior', representation='F#', is_minor=0),
+        Chord(name='Sol Maior', representation='G', is_minor=0),
+        Chord(name='Sol Sustenido Maior', representation='G#', is_minor=0),
+        Chord(name='La Maior', representation='A', is_minor=0),
+        Chord(name='La Sustenido Maior', representation='A#', is_minor=0),
+        Chord(name='Si Maior', representation='B', is_minor=0),
+        Chord(name='Do Menor', representation='Cm', is_minor=1),
+        Chord(name='Do Sustenido Menor', representation='C#m', is_minor=1),
+        Chord(name='Re Menor', representation='Dm', is_minor=1),
+        Chord(name='Re Sustenido Menor', representation='D#m', is_minor=1),
+        Chord(name='Mi Menor', representation='Em', is_minor=1),
+        Chord(name='Fa Menor', representation='Fm', is_minor=1),
+        Chord(name='Fa Sustenido Menor', representation='F#m', is_minor=1),
+        Chord(name='Sol Menor', representation='Gm', is_minor=1),
+        Chord(name='Sol Sustenido Menor', representation='G#m', is_minor=1),
+        Chord(name='La Menor', representation='Am', is_minor=1),
+        Chord(name='La Sustenido Menor', representation='A#m', is_minor=1),
+        Chord(name='Si Menor', representation='Bm', is_minor=1)
+    ]
+
+    for i in range(len(chords_list)):
+        db.session.add(chords_list[i])
+    db.session.commit()
+    return "Chords successfully loaded"
+
+
+@app.route('/load_test_user', methods=['GET', 'POST'])
+def load_test_user():
+    if request.method == 'GET':
+        return render_template('superuser_password_form.html')
+    elif request.method == 'POST':
+        if request.form['password'] == "123456":
+            if User.query.filter_by(email="teste@teste").first() != None:
+                return "SUPER USER ALREADY EXISTS"
+            superuser = User(first_name="super",last_name="user",scout_group=1234,email="teste@teste",password="123456")
+            db.session.add(superuser)
+            db.session.commit()
+            return "SUPER USER SUCCESSFULLY ADDED"
+        return "WRONG PASSWORD"
+
+@app.route('/api=get_available_songs', methods=['GET'])
+def get_available_songs():
+    res = {}
+    songs = Song.query.all()
+    for i in range(len(songs)):
+        id = songs[i].id
+        name = songs[i].title
+        res[id] = name
+    return json.dumps({"result": "SUCCESS", "data": res})
+
+
+@app.route('/api=get_song_by_id', methods=['GET','POST'])
+def get_song_by_id():
+    if request.method == "GET":
+        return render_template('get_song_by_id.html')
+    elif request.method == "POST":
+        song_id = request.form['id']
+        song = Song.query.filter_by(id=song_id).first()
+        return json.dumps({"result": "SUCCESS", "data": song.__repr__()})
+
+
+def check_profanity(args):
+    """
+    Check if song title or song lyrics contains bad words
+    :param title: title of the song
+    :param lyrics: lyrics of the song
+    :return: True if it contains any bad words
+    """
+    censor = False
+    for text in args:
+        if isinstance(text, list):
+            for line in text:
+                if profanity.contains_profanity(line):
+                    censor = True
+        elif isinstance(text, str):
+            if profanity.contains_profanity(text):
+                censor = True
+        else:
+            raise ValueError("not list nor text. type='{}'".format(text.__class__))
+    return censor
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,22 +1,87 @@
+import json
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
-class InfoModel(db.Model):
-    __tablename__ = 'info_table'
+class User(db.Model):
+    __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), unique=True)
-    age = db.Column(db.Integer())
-    password = db.Column(db.String())
-    email = db.Column(db.String(), unique=True)
+    first_name = db.Column(db.String(), nullable=False)
+    last_name = db.Column(db.String(), nullable=False)
+    scout_group = db.Column(db.Integer(), nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    is_admin = db.Column(db.Integer(), nullable=False)
 
-    def __init__(self, name, age, email, password):
-        self.name = name
-        self.age = age
+    def __init__(self, first_name, last_name, scout_group, email, password, is_admin):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.scout_group = scout_group
         self.email = email
         self.password = password
+        self.is_admin = is_admin
 
     def __repr__(self):
-        return f"{self.name}:{self.age}"
+        return json.dumps({
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "scout_group": self.scout_group,
+            "password": self.password,
+            'is_admin': self.is_admin
+        })
+
+
+class Chord(db.Model):
+    __tablename__ = "chord"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    representation = db.Column(db.String(), nullable=False)
+    is_minor = db.Column(db.Integer(), nullable=False)
+#    image = db.Column(db.Image)??????
+
+    def __init__(self, name, is_minor, representation):
+        self.name = name
+        self.is_minor = is_minor
+        self.representation = representation
+
+    def __repr__(self):
+        return json.dumps({
+            "name": self.name,
+            "is_minor": self.is_minor,
+            "representation": self.representation
+        })
+
+
+class Song(db.Model):
+    __tablename__ = "song"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    lyrics = db.Column(db.JSON(), nullable=False)
+    chords_list = db.Column(db.JSON())
+    categories = db.Column(db.JSON())
+    creator_id = db.Column(db.Integer())
+    is_verified = db.Column(db.Integer())
+
+    def __init__(self,title,lyrics,chords_list,categories,creator_id):
+        self.title = title
+        self.lyrics = lyrics
+        self.chords_list = chords_list
+        self.categories = categories
+        self.creator_id = creator_id
+        self.is_verified = 0    # not verified by default
+
+    def __repr__(self):
+        return json.dumps({
+            "title": self.title,
+            "lyrics": self.lyrics,
+            "chords_list": self.chords_list,
+            "categories": self.categories,
+            "creator_id": self.creator_id,
+            "is_verified": self.is_verified
+        })
