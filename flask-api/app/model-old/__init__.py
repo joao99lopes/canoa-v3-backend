27bee5575221f55a6390646d1cd6e7b40e274e7b
lopesj <joao.lopes@studentlab.pt>
2022-12-21 17:30:50 +0000

    small changes

--- flask-api/app/model-old/__init__.py
+++
@@ -1,5 +0,0 @@
-# from .song import *
-# from .chord import *
-# from .user import *
-# from .relations import *
-# from .playlist import *
--- flask-api/test-files/test.py
+++ flask-api/test-files/test.py
@@ -4,6 +4,14 @@ test = {
     'title': 'this is the title',
     'lyrics': 'omg this a song is so wow again'
 }
+
+# test = {
+#     'first_name': 'Maria',
+#     'last_name': 'Balsinhas',
+#     'email': 'drabrinquedos@gmail.com',
+#     'password': 'password'
+# }
+
 x = requests.post(f'{base_url}song/new', json = test, )
 
 print(x.text)
