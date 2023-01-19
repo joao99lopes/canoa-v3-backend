e8aff4726baed62c08ab091e3009510c299b2ef7
joao99lopes <joao.aclopes99@gmail.com>
2023-01-19 16:52:30 +0000

    changes

--- flask-api/app/config.py
+++ flask-api/app/config.py
@@ -1,8 +1,7 @@
 class DBConfig():
-    #DB_USER = 'read_only_dev'
-    #DB_PASSWORD = '64mXtiKtwUgJYe2B'
     DB_USER = 'postgres'
     DB_PASSWORD = 'postgres'
+    # DB_ADDRESS = '192.168.1.206'
     DB_ADDRESS = '192.168.1.206'
     DB_NAME = 'scoutify_db'
     SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}/{DB_NAME}'
